31c12a18ac1326c7321bf7e43e84bfc5a22f50a5
bouzidanas <anasbouzid@gmail.com>
2024-08-31 05:19:58 -0500

    fix theme not overriding Page theme

--- dist/components/PageSection.js
+++ dist/components/PageSection.js
@@ -1,9 +1,9 @@
 import { jsx as A } from "react/jsx-runtime";
 import { forwardRef as D, useContext as O } from "react";
 import { PageContext as q } from "./Page.js";
-import '../css/PageSection.css';const G = D(({ height: n, gap: i, padding: s, bgColor: e, scrollBottom: r, scrollTop: m, vertical: y, light: d, dark: f, centerText: o, clip: _, maxWidth: a, maxHeight: v, minHeight: u, z: S, fontScale: c, radius: N, top: j, bottom: w, className: p, style: l, id: I, children: M }, P) => {
+import '../css/PageSection.css';const G = D(({ height: n, gap: i, padding: s, bgColor: e, scrollBottom: r, scrollTop: m, vertical: v, light: y, dark: f, centerText: l, clip: _, maxWidth: a, maxHeight: d, minHeight: u, z: S, fontScale: c, radius: N, top: j, bottom: w, className: p, style: o, id: I, children: M }, P) => {
   const t = O(q);
-  Object.keys(t).length !== 0 && (n = n ?? t.height, i = i ?? t.gap, s = s ?? t.padding, e = e ?? t.bgColor, r = r ?? t.scrollBottom, m = m ?? t.scrollTop, y = y ?? t.vertical, d = d ?? f === void 0 ? t.light : void 0, f = f ?? d === void 0 ? t.dark : void 0, o = o ?? t.centerText, a = a ?? t.maxWidth, v = v ?? t.maxHeight, u = u ?? t.minHeight, c = c ?? t.fontScale, p = p ?? t.className, l = l ?? t.style);
+  Object.keys(t).length !== 0 && (n = n ?? t.height, i = i ?? t.gap, s = s ?? t.padding, e = e ?? t.bgColor, r = r ?? t.scrollBottom, m = m ?? t.scrollTop, v = v ?? t.vertical, y = y ?? f ? !1 : t.light, f = f ?? t.dark, l = l ?? t.centerText, a = a ?? t.maxWidth, d = d ?? t.maxHeight, u = u ?? t.minHeight, c = c ?? t.fontScale, p = p ?? t.className, o = o ?? t.style);
   const R = t.className ?? "", $ = t.style ?? {};
   typeof e == "number" ? e = t.bgColorSet ? t.bgColorSet[e] : void 0 : Array.isArray(e) && (e = e.map((x) => typeof x == "number" ? t.bgColorSet ? t.bgColorSet[x] : "undefined" : x), e.includes("undefined") && (e = void 0)), n = n ?? "fit", i = i ?? "1.3rem", s = s ?? "0", e = e ?? "transparent";
   const k = {
@@ -20,15 +20,15 @@ import '../css/PageSection.css';const G = D(({ height: n, gap: i, padding: s, bg
     zIndex: S,
     overflow: _ ? "hidden" : void 0
   }, z = {
-    maxHeight: v === "full" ? "100%" : v,
+    maxHeight: d === "full" ? "100%" : d,
     maxWidth: a === "full" ? void 0 : a,
-    flexDirection: y ? "column" : void 0,
+    flexDirection: v ? "column" : void 0,
     gap: i,
-    textAlign: o ? "center" : void 0,
-    ...l,
+    textAlign: l ? "center" : void 0,
+    ...o,
     ...$
   };
-  return /* @__PURE__ */ A("section", { id: I, ref: P, className: "page__section " + R + " " + (p ?? ""), style: k, "data-theme": d ? "light" : f ? "dark" : void 0, children: /* @__PURE__ */ A("div", { className: "section__content", style: z, children: M }) });
+  return /* @__PURE__ */ A("section", { id: I, ref: P, className: "page__section " + R + " " + (p ?? ""), style: k, "data-theme": y ? "light" : f ? "dark" : void 0, children: /* @__PURE__ */ A("div", { className: "section__content", style: z, children: M }) });
 });
 export {
   G as default
